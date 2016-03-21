ff2ccbd1438206fdff726d6e43b5dc41f9d19a59
HIroshi Takagi <gollum.hiroshi@gmail.com>
2016-03-21 09:36:14 +0000

    add restriction to uptdate profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+
   def show
    @user = User.find(params[:id])
   end
@@ -18,9 +20,26 @@ class UsersController < ApplicationController
   end
 
   def edit
+    unless current_user == @user
+      flash[:danger] = "access denied: please login as right user"
+      redirect_to login_url
+    end
   end
 
   def update
+    if current_user == @user
+      @user.update(user_params)
+      if @user.save
+        flash[:success] = "Your profile has been updated."
+        redirect_to @user
+      else
+        flash[:alert] = "failed to update profile."
+        render 'edit'
+      end
+    else
+      flash[:danger] = "access denied: please login as right user"
+      redirect_to login_url
+    end
   end
 
   private
@@ -29,5 +48,9 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation,
                                  :location, :profile)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/edit'
-
-  get 'users/update'
-
   root to: 'static_pages#home'
   get    'signup', to: 'users#new'
   get    'login' , to: 'sessions#new'
