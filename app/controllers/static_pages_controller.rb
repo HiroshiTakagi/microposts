c8d4923fb0bc5d204da01f1ba068abb424ff0bf2
HIroshi Takagi <gollum.hiroshi@gmail.com>
2016-03-26 15:53:16 +0000

    correct routes. make them explicit

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -13,14 +13,16 @@ class StaticPagesController < ApplicationController
       @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
 
       @search_keywords =  SearchKeywords.new(params.require(:search_keywords))
-      @keywords = @search_keywords.keywords.gsub(/　/, ' ').split
-      @keywords.each_with_index do |keyword, i|
-        if i == 0
-          @microposts_found  = Micropost.where("content LIKE ?", "%#{keyword}%")
-        else
-          @microposts_found  = @microposts_found.where("content LIKE ?", "%#{keyword}%")
-        end
-      end 
+      unless @search_keywords.keywords.strip == ''
+        @keywords = @search_keywords.keywords.gsub(/　/, ' ').split
+        @keywords.each_with_index do |keyword, i|
+          if i == 0
+            @microposts_found  = Micropost.where("content LIKE ?", "%#{keyword}%")
+          else
+            @microposts_found  = @microposts_found.where("content LIKE ?", "%#{keyword}%")
+          end
+        end 
+      end
       render :action =>'home'
     end
   end
--- app/models/search_keywords.rb
+++ app/models/search_keywords.rb
@@ -1,4 +1,6 @@
 class SearchKeywords
   include ActiveModel::Model
   attr_accessor :keywords
+  
+  validates :keywords, presence: true,  length: { minimum: 1 }
 end
